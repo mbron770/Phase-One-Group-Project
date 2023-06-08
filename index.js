6d65c94e2310d80a32d160e05b1f16eaaeeb9680
Ryan Sinnott <ryan.christopher.sinnott@gmail.com>
2023-06-08 00:40:02 -0400

    get patch event listener for click button

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
+const URL = "http://localhost:3000/cart/1"
+let cartQuantity
+let coinNameInCart
+let cartTotal
 
+function init(URL){
+
+function fetchFunction(URL){
+    fetch(URL)
+    .then(response=>response.json())
+    .then(cart=>printCartDetails(cart))
+    .catch(error => alert(error.message))
+}
+fetchFunction(URL)
+
+function updateCart(URL, cartQuantity, coinNameInCart, cartTotal) {
+    fetch(URL,{ 
+    method: "PATCH",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({quantity: cartQuantity, names: coinNameInCart, total: cartTotal})
+})
+.then(response => response.json())
+.then(cart => printCartDetails(cart))
+.catch(error => alert(error.message))
+}
+updateCart(URL, cartQuantity, coinNameInCart, cartTotal)
+function printCartDetails(cart){
+    cartQuantity = cart.quantity
+    coinNameInCart = cart.names
+    cartTotal = cart.total
+
+    document.querySelector('#nameInCart').textContent = `Coin: ${cart.names}`
+    document.querySelector('#amountInCart').textContent = `Total Quantity: ${cart.quantity}`
+    document.querySelector('#shoppingCartAmount').textContent = cart.quantity
+    document.querySelector('#totalInCart').textContent = `Total: $${cart.total}`
+}
+
+
+document.querySelector("#addToCartInCardButton").addEventListener("click", (e)=>{
+    e.preventDefault()
+  updateCart(URL, cartQuantity, coinNameInCart, cartTotal)
+})
+
+
+}
+
+init(URL)
